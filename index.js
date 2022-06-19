251e7cadacbefd6f633a5799ce377d8ef9bbbe8b
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-06-19 17:12:21 -0300

    Requesitos bônus

--- index.js
+++ index.js
@@ -11,26 +11,53 @@ const tweets = [];
 
 server.post('/sign-up', (request, response) => {
     const user = request.body;
-    users.push(user);
-    response.send('OK');
+    if (!user.username|| !user.avatar) {
+        response.status(400).send('Todos os campos são obrigatórios!');
+        return;
+    }   else {
+        users.push(user);
+        response.status(201).send('OK');
+        return;
+    }
+  
 });
 
 server.get('/tweets', (request, response) => {
-    const lastTenTweets = tweets.slice(-10);
+    const id = request.query.page;
+    if (id < 1 || id === null) {
+        response.status(400).send('Informe uma página válida!');
+        return;
+    }
+    let lastTenTweets;
+    if (parseInt(id) === 1) lastTenTweets = tweets.slice(-10);
+    else lastTenTweets = tweets.slice(-10 * id, -10 * (id -1));
     lastTenTweets.forEach( tweet => tweet.avatar = users.find( user => user.username === tweet.username).avatar);
-    response.send(lastTenTweets.reverse());
+    response.status(201).send(lastTenTweets.reverse());
 });
 
 server.post('/tweets', (request, response) => {
-    const tweet = request.body;
-    tweets.push(tweet);
-    response.send('OK');
+    const { tweet } = request.body;
+    const user = request.headers['user'];
+    if (!tweet || !user) {
+        response.status(400).send('Todos os campos são obrigatórios"');
+        return;
+    }   else {
+        tweets.push({
+            username: user,
+            tweet,
+        });
+        response.status(201).send('OK');
+        return;
+    }
 });
 
+server.get('/tweets/:idUser', (request, response) => {
+    const user = request.params.idUser;
+    const userTweets = tweets.filter( tweet => tweet.username === user);
+    userTweets.forEach( tweet => tweet.username = user);
+    response.send(userTweets);
+})
 
-
-
-
-
-
-server.listen(5000, console.log('Servidor no ar!'));
+server.listen(5000, () => {
+    console.log('Servidor no ar!');
+});
